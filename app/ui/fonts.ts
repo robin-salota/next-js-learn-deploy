d9e8ad99e8953c38d577ccf99d4492b8c5c5ba56
Robin <58702076+robin-salota@users.noreply.github.com>
2024-12-06 13:53:09 -0500

    Font Fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import {Inter} from 'next/font/google';
-export const inter = Inter({subsets: ['Latin']});
+export const inter = Inter({subsets: ['latin']});
 
 import { Lusitana } from 'next/font/google';
 export const lusitana  = Lusitana({
