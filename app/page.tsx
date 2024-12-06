40ca56042336f5d8d71e5b97b6a91561607a1418
Robin <58702076+robin-salota@users.noreply.github.com>
2024-12-06 15:38:42 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
-import { Lustria } from 'next/font/google';
 import Image from 'next/image';
+import 
 
 export default function Page() {
   return (
